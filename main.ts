8ce7f49f0b0ed4043174b5322b749779af3b2d9f
FacePunch119 <92817162+FacePunch119@users.noreply.github.com>
2021-12-13 14:18:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,9 @@
-radio.onReceivedString(function (receivedString) {
-    while (true) {
-        basic.showString(receivedString)
-        for (let index = 0; index < 4; index++) {
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-            basic.clearScreen()
-            basic.pause(500)
-            basic.pause(250)
-        }
-    }
-})
 basic.forever(function () {
     let Pressure = 0
     radio.setGroup(130)
-    if (Pressure < 180) {
+    if (Pressure < 500) {
         basic.showIcon(IconNames.Yes)
-    } else if (Pressure > 180) {
+    } else if (Pressure > 500) {
         while (true) {
             radio.sendString("Intruder!")
         }
